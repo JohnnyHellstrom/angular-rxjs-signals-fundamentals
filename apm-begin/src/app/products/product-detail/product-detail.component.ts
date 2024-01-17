c35bccb59ef03e521581935c8e6a35f7d56ea71c
JohnnyHellstrom <124794710+JohnnyHellstrom@users.noreply.github.com>
2024-01-17 22:51:13 +0100

    end of day, finished module 8

--- apm-begin/src/app/products/product-detail/product-detail.component.ts
+++ apm-begin/src/app/products/product-detail/product-detail.component.ts
@@ -1,25 +1,57 @@
-import { Component, Input } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnDestroy,
+  SimpleChanges,
+  inject,
+} from '@angular/core';
 
 import { NgIf, NgFor, CurrencyPipe } from '@angular/common';
 import { Product } from '../product';
+import { ProductService } from '../product.service';
+import { EMPTY, Subscription, catchError } from 'rxjs';
 
 @Component({
-    selector: 'pm-product-detail',
-    templateUrl: './product-detail.component.html',
-    standalone: true,
-    imports: [NgIf, NgFor, CurrencyPipe]
+  selector: 'pm-product-detail',
+  templateUrl: './product-detail.component.html',
+  standalone: true,
+  imports: [NgIf, NgFor, CurrencyPipe],
 })
-export class ProductDetailComponent {
-  // Just enough here for the template to compile
+export class ProductDetailComponent implements OnChanges, OnDestroy {
   @Input() productId: number = 0;
   errorMessage = '';
+  sub!: Subscription;
+
+  private productService = inject(ProductService);
 
   // Product to display
   product: Product | null = null;
 
   // Set the page title
-  pageTitle = this.product ? `Product Detail for: ${this.product.productName}` : 'Product Detail';
+  pageTitle = this.product
+    ? `Product Detail for: ${this.product.productName}`
+    : 'Product Detail';
 
-  addToCart(product: Product) {
+  ngOnChanges(changes: SimpleChanges): void {
+    const id = changes['productId'].currentValue;
+    if (id) {
+      this.sub = this.productService
+        .getProduct(id)
+        .pipe(
+          catchError((err) => {
+            this.errorMessage = err;
+            return EMPTY;
+          })
+        )
+        .subscribe((product) => (this.product = product));
+    }
   }
+  ngOnDestroy(): void {
+    if (this.sub) {
+      this.sub.unsubscribe();
+    }
+  }
+
+  addToCart(product: Product) {}
 }
--- apm-begin/src/app/products/product.service.ts
+++ apm-begin/src/app/products/product.service.ts
@@ -1,10 +1,59 @@
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Product } from './product';
+import { HttpErrorService } from '../utilities/http-error.service';
+import { ReviewService } from '../reviews/review.service';
+import { Review } from '../reviews/review';
+import {
+  Observable,
+  catchError,
+  map,
+  of,
+  shareReplay,
+  switchMap,
+  tap,
+  throwError,
+} from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
-  // Just enough here for the code to compile
   private productsUrl = 'api/products';
 
+  private http = inject(HttpClient);
+  private errorService = inject(HttpErrorService);
+  private reviewService = inject(ReviewService);
+
+  readonly products$ = this.http.get<Product[]>(this.productsUrl).pipe(
+    tap((p) => console.log(JSON.stringify(p))),
+    shareReplay(1),
+    catchError((err) => this.handleError(err))
+  );
+
+  getProduct(id: number): Observable<Product> {
+    const productUrl = this.productsUrl + '/' + id;
+    return this.http.get<Product>(productUrl).pipe(
+      tap(() => console.log('in http get by id pipeline')),
+      switchMap((product) => this.getProductReviews(product)),
+      catchError((err) => this.handleError(err))
+    );
+  }
+
+  private getProductReviews(product: Product): Observable<Product> {
+    if (product.hasReviews) {
+      return this.http
+        .get<Review[]>(this.reviewService.getReviewUrl(product.id))
+        .pipe(map((reviews) => ({ ...product, reviews } as Product)));
+    } else {
+      return of(product);
+    }
+  }
+
+  private handleError(err: HttpErrorResponse): Observable<never> {
+    const formattedMsg = this.errorService.formatError(err);
+
+    return throwError(() => formattedMsg);
+    //throw formattedMsg;
+  }
 }
